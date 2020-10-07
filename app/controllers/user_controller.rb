32b5c78e5882b0ff10341d239d8b96ae354786c6
yurigurlie207 <adrienne.yao@gmail.com>
2020-10-07 12:16:21 -0400

    Added new, create and user params limits to user controller

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -1,5 +1,22 @@
 class UserController < ApplicationController
 
+    def new
+    end
+  
+    def create
+      @user = User.create(user_params)
+      return redirect_to controller: 'users', action: 'new' unless @user.save
+      session[:user_id] = @user.id
+      redirect_to controller: 'welcome', action: 'home'
+    end
+  
+    private
+  
+    def user_params
+      params.require(:user).permit(:name, :password, :password_confirmation)
+    end
+
+
   get '/user' do
     erb :'/user/index'
   end
