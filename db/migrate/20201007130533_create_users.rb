017341a7f97305f3fcbadd3d5b788bd71eee30f0
yurigurlie207 <adrienne.yao@gmail.com>
2020-10-07 10:17:16 -0400

    Brought in columns of user table from Sinatra project

--- db/migrate/20201007130533_create_users.rb
+++ db/migrate/20201007130533_create_users.rb
@@ -1,6 +1,15 @@
 class CreateUsers < ActiveRecord::Migration[6.0]
   def change
     create_table :users do |t|
+          t.string :username
+          t.text :email
+          t.string :password_digest
+          t.string :firstname
+          t.string :lastname
+          t.string :role
+          t.date :birthdate
+        end
+      end
 
       t.timestamps
     end
