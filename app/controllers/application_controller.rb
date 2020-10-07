950877d109d9b14d70c8c3ae678179956c38305a
yurigurlie207 <adrienne.yao@gmail.com>
2020-10-07 12:27:13 -0400

    Added some application controller code from rails lab

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -33,4 +33,23 @@ class ApplicationController < ActionController::Base
         end
     
       end
+
+      end
+
+      protect_from_forgery with: :exception
+
+  before_action :current_user
+
+  def current_user
+    @user = (User.find_by(id: session[:user_id]) || User.new)
+  end
+
+  def logged_in?
+    current_user.id != nil
+  end
+
+  def require_logged_in
+    return redirect_to(controller: 'sessions', action: 'new') unless logged_in?
+  end
+
 end
