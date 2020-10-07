d5c2793335d7c3f9fec2949128d298923d1046e2
yurigurlie207 <adrienne.yao@gmail.com>
2020-10-07 09:33:38 -0400

    Brought in application controller code from Sinatra project

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,36 @@
 class ApplicationController < ActionController::Base
+    configure do
+        set :public_folder, 'public'
+        set :views, 'app/views'
+        enable :sessions
+        register Sinatra::Flash
+        set :session_secret, "task_secret"
+      end
+    
+      get '/' do
+        erb :index
+      end
+    
+      helpers do
+        def logged_in?
+          !!current_user
+        end
+    
+        def current_user
+          @current_user ||= User.find_by(id: session[:user_id]) if session[:user_id]
+        end
+    
+        def can_edit?(subtasks)
+          subtasks.each do |subtask|
+            subtask.users.each do |user|
+                if session[:user_id] == user.id
+                    @can_edit = true
+                 end
+              end
+          end
+    
+          @can_edit ||= false
+        end
+    
+      end
 end
