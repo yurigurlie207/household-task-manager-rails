8ff65dcdc6adcb8761ce792dcfe367b0f61219a4
yurigurlie207 <adrienne.yao@gmail.com>
2020-10-07 12:29:50 -0400

    Added in the resource line for users, will refactor later

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-
+  resources :users, only: [:new, :create]
+  
   get '/users/new', to: 'users#new', as: 'new_user'
   root 'static#home'
   get '/users', to:'users#index', as: 'users'
