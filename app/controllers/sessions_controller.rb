2fcdb9d1d51f206cee66f812ee2f4ff3732bd9bc
yurigurlie207 <adrienne.yao@gmail.com>
2020-10-08 09:27:36 -0400

    Added in the necessary sessions code for omni auth; still needs to be cleaned up and refactored

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,24 +1,43 @@
 class SessionsController < ApplicationController
+  skip_before_action :verify_authenticity_token, only: :create
+
     def new
     end
 
     def create
-        user = User.find_by(name: params[:user][:name])
-    
-        user = user.try(:authenticate, params[:user][:password])
-    
-        return redirect_to(controller: 'sessions', action: 'new') unless user
-    
-        session[:user_id] = user.id
-    
-        @user = user
-    
-        redirect_to controller: 'welcome', action: 'home'
-      end
-    
-      def destroy
-        session.delete :user_id
-    
-        redirect_to '/'
-      end
+      user = User.find_by(name: params[:user][:name])
+  
+      user = user.try(:authenticate, params[:user][:password])
+  
+      return redirect_to(controller: 'sessions', action: 'new') unless user
+  
+      session[:user_id] = user.id
+  
+      @user = user
+  
+      redirect_to controller: 'welcome', action: 'home'
+
+
+    # After entering a name and email value in the /auth/developer
+    # path and submitting the form, you will see a pretty-print of
+    # the authentication data object that comes from the "developer"
+    # strategy. In production, we'll swap this strategy for something
+    # like 'github' or 'facebook' or some other authentication broker
+    pp request.env['omniauth.auth']
+
+    # We're going to save the authentication information in the session
+    # for demonstration purposes. We want to keep this data somewhere so that,
+    # after redirect, we have access to the returned data
+    session[:name] = request.env['omniauth.auth']['info']['name']
+    session[:omniauth_data] = request.env['omniauth.auth']
+
+    # Ye olde redirect
+    redirect_to root_path
+    end
+  
+    def destroy
+      session.delete :user_id
+  
+      redirect_to '/'
+    end
 end
