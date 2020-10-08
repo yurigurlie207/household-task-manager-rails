81305e01e047475b3f69260be62ed8f630f72a98
yurigurlie207 <adrienne.yao@gmail.com>
2020-10-08 09:06:50 -0400

    Cleaned up application ccontroller so the can_edit task is commented out for now, and deleted some redundant methods

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,38 +1,20 @@
 class ApplicationController < ActionController::Base
-
-      get '/' do
-        erb :index
-      end
-    
-      helpers do
-        def logged_in?
-          !!current_user
-        end
-    
-        def current_user
-          @current_user ||= User.find_by(id: session[:user_id]) if session[:user_id]
-        end
-    
-        def can_edit?(subtasks)
-          subtasks.each do |subtask|
-            subtask.users.each do |user|
-                if session[:user_id] == user.id
-                    @can_edit = true
-                 end
-              end
-          end
-    
-          @can_edit ||= false
-        end
-    
-      end
-
-      end
-
-      protect_from_forgery with: :exception
-
+  protect_from_forgery with: :exception
   before_action :current_user
 
+    
+  # def can_edit?(subtasks)
+  #   subtasks.each do |subtask|
+  #     subtask.users.each do |user|
+  #         if session[:user_id] == user.id
+  #             @can_edit = true
+  #           end
+  #       end
+  #   end
+
+  #   @can_edit ||= false
+  # end
+  
   def current_user
     @user = (User.find_by(id: session[:user_id]) || User.new)
   end
