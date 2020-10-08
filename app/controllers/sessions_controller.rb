da9702b6d7a04e5582684a494fec694cda8bdb3c
yurigurlie207 <adrienne.yao@gmail.com>
2020-10-08 09:18:42 -0400

    Deleted some redundant lines in sessions controller

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,18 +1,6 @@
 class SessionsController < ApplicationController
     def new
     end
-  
-    def create
-      return redirect_to(controller: 'sessions',
-                         action: 'new') if !params[:name] || params[:name].empty?
-      session[:name] = params[:name]
-      redirect_to controller: 'application', action: 'hello'
-    end
-  
-    def destroy
-      session.delete :name
-      redirect_to controller: 'application', action: 'hello'
-    end
 
     def create
         user = User.find_by(name: params[:user][:name])
