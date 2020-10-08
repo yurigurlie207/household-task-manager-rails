56dd04e2c1160d029495e1a36b781407a736d37b
yurigurlie207 <adrienne.yao@gmail.com>
2020-10-08 11:18:40 -0400

    Commented out the other user methods for now in controller

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -5,9 +5,6 @@ class UserController < ApplicationController
     def index
     end
 
-    def userhome
-    end
-
     def new
     end
   
@@ -17,117 +14,120 @@ class UserController < ApplicationController
       session[:user_id] = @user.id
       redirect_to controller: 'welcome', action: 'home'
     end
+
+    def userhome
+    end
   
-
-
-
-  get '/user/signup' do
-    if !logged_in?
-      erb :'/user/signup'
-    else
-      redirect to '/user/userhome'
+    private
+  
+    def user_params
+      params.require(:user).permit(:name, :password, :password_confirmation)
     end
-  end
-
-  get '/user/login' do
-    if !logged_in?
-      erb :'/user/login'
-    else
-      redirect to '/user/userhome'
+   
+    def require_login
+      return head(:forbidden) unless session.include? :user_id
     end
-  end
 
-  # get '/user/userhome' do
+
+  # get '/user/signup' do
+  #   if !logged_in?
+  #     erb :'/user/signup'
+  #   else
+  #     redirect to '/user/userhome'
+  #   end
+  # end
+
+  # get '/user/login' do
+  #   if !logged_in?
+  #     erb :'/user/login'
+  #   else
+  #     redirect to '/user/userhome'
+  #   end
+  # end
+
+  # # get '/user/userhome' do
+  # #   if !logged_in?
+  # #     erb :'/user/login'
+  # #   else
+  # #     @user = current_user
+  # #     erb :'/user/userhome'
+  # #   end
+  # # end
+
+  # post '/signup' do
+  #   usernames = User.all.collect { |user| user.username }
+
+  #   if params['user']['username'] == "" || params['user']['password'] == ""
+  #     flash.now[:error] = "please make sure you have a username and password"
+  #     erb :'/user/signup'
+  #   elsif usernames.include?(params['user']['username'])
+  #     flash.now[:error] = "username already exists"
+  #     erb :'/user/signup'
+  #   else
+  #     @user = User.create(params["user"])
+  #     session[:user_id] = @user.id
+  #     redirect to "/user/#{@user.id}"
+  #   end
+  # end
+
+  # post '/login' do
+  #  user = User.find_by(:username => params['user']['username'])
+  #  if user && user.authenticate(params['user']['password'])
+  #    session[:user_id] = user.id
+  #    redirect to "/user/userhome"
+  #  else
+  #    flash.now[:error] = "incorrect login or password"
+  #    erb :'/user/login'
+  #  end
+  # end
+
+  # get '/user/:id/edit' do
   #   if !logged_in?
   #     erb :'/user/login'
   #   else
   #     @user = current_user
-  #     erb :'/user/userhome'
+  #     erb :'/user/edit'
   #   end
   # end
 
-  post '/signup' do
-    usernames = User.all.collect { |user| user.username }
+  # patch '/user/:id' do
+  #   if !logged_in?
+  #     erb :'/user/login'
+  #   else
+  #     @user = current_user
+  #     @user.update(params['user'])
+  #     redirect to "/user/#{@user.id}"
+  #   end
+  # end
 
-    if params['user']['username'] == "" || params['user']['password'] == ""
-      flash.now[:error] = "please make sure you have a username and password"
-      erb :'/user/signup'
-    elsif usernames.include?(params['user']['username'])
-      flash.now[:error] = "username already exists"
-      erb :'/user/signup'
-    else
-      @user = User.create(params["user"])
-      session[:user_id] = @user.id
-      redirect to "/user/#{@user.id}"
-    end
-  end
+  # delete '/user/:id/delete' do
+  #    if !logged_in?
+  #      erb :'/user/login'
+  #   else
+  #     @user = current_user
+  #     @user.delete
+  #     redirect to '/user/signup'
+  #   end
+  # end
 
-  post '/login' do
-   user = User.find_by(:username => params['user']['username'])
-   if user && user.authenticate(params['user']['password'])
-     session[:user_id] = user.id
-     redirect to "/user/userhome"
-   else
-     flash.now[:error] = "incorrect login or password"
-     erb :'/user/login'
-   end
-  end
+  # get '/user/logout' do
+  #     if logged_in?
+  #       session.destroy
+  #       redirect to '/user/login'
+  #     else
+  #       redirect to '/'
+  #     end
+  # end
 
-  get '/user/:id/edit' do
-    if !logged_in?
-      erb :'/user/login'
-    else
-      @user = current_user
-      erb :'/user/edit'
-    end
-  end
+  # get '/user/:id' do
+  #   if !logged_in?
+  #     erb :'/user/login'
+  #    else
+  #      #This is really a route feature for debugging, so current user helper not used
+  #       @user = User.find_by_id(params[:id])
+  #       erb :'/user/profile'
+  #   end
+  # end
 
-  patch '/user/:id' do
-    if !logged_in?
-      erb :'/user/login'
-    else
-      @user = current_user
-      @user.update(params['user'])
-      redirect to "/user/#{@user.id}"
-    end
-  end
 
-  delete '/user/:id/delete' do
-     if !logged_in?
-       erb :'/user/login'
-    else
-      @user = current_user
-      @user.delete
-      redirect to '/user/signup'
-    end
-  end
-
-  get '/user/logout' do
-      if logged_in?
-        session.destroy
-        redirect to '/user/login'
-      else
-        redirect to '/'
-      end
-  end
-
-  get '/user/:id' do
-    if !logged_in?
-      erb :'/user/login'
-     else
-       #This is really a route feature for debugging, so current user helper not used
-        @user = User.find_by_id(params[:id])
-        erb :'/user/profile'
-    end
-  end
-
-  private
-  
-  def user_params
-    params.require(:user).permit(:name, :password, :password_confirmation)
-  end
- 
-  def require_login
-    return head(:forbidden) unless session.include? :user_id
-  end
 end
