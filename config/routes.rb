9a15c84ba988e5a46f95ab35cea37e3cf4046e1e
yurigurlie207 <adrienne.yao@gmail.com>
2020-10-11 09:54:52 -0400

    Updated links in app home page, and fixed some naming conventions in routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,14 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   resources :users, only: [:new, :create]
-  
+
   root 'static#home'
 
-  get '/users/new', to: 'users#new', as: 'new_user'
-  get '/users', to:'users#index', as: 'users'
-  post '/users', to:'users#create'
-  get '/users/:id', to: 'users#show', as: 'user'
-  get '/users/:id/edit', to: 'users#edit', as: 'edit_user'
+  get '/user/new', to: 'user#new', as: 'new_user'
+  get '/user', to:'user#index', as: 'users'
+  post '/user', to:'user#create'
+  get '/user/:id', to: 'user#show', as: 'user'
+  get '/user/:id/edit', to: 'user#edit', as: 'edit_user'
 
   get '/signin', to: 'session#new', as: 'signin'
   post '/login' => 'sessions#create'
@@ -19,7 +19,5 @@ Rails.application.routes.draw do
   # post '/logout' => 'sessions#destroy'
 
   match '/auth/:provider/callback', to: 'sessions#create', via: [:get, :post]
-
-
 
 end
