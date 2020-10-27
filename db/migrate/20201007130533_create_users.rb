64b029b3cf5a775eb25caf3c8ecb3f07c4054cf4
yurigurlie207 <adrienne.yao@gmail.com>
2020-10-27 10:15:15 -0400

    Deleted redundant user route, and ran user table migration

--- db/migrate/20201007130533_create_users.rb
+++ db/migrate/20201007130533_create_users.rb
@@ -1,4 +1,5 @@
 class CreateUsers < ActiveRecord::Migration[6.0]
+  
   def change
     create_table :users do |t|
           t.string :username
@@ -8,10 +9,8 @@ class CreateUsers < ActiveRecord::Migration[6.0]
           t.string :lastname
           t.string :role
           t.date :birthdate
-        end
+          t.timestamps
       end
-
-      t.timestamps
     end
-  end
+
 end
