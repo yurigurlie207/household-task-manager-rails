0e5535c95be4c060f522387ca7b6bd883a006eac
yurigurlie207 <adrienne.yao@gmail.com>
2020-11-14 10:29:51 -0500

    Updated foreign key attributes in user task join table

--- db/migrate/20201114150847_create_user_tasks.rb
+++ db/migrate/20201114150847_create_user_tasks.rb
@@ -1,9 +1,9 @@
 class CreateUserTasks < ActiveRecord::Migration[6.0]
   def change
     create_table :user_tasks do |t|
-      t.integer :user_id
-      t.integer :subtask_id
-      t.timestamps
+      t.belongs_to :user, index: true, foreign_key: true
+      t.belongs_to :subtask, index: true, foreign_key: true
+      t.timestamps null: false
     end
   end
 end
