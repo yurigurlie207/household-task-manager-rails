792b9cf32ee8c751dda15477a4ba549b37501c25
yurigurlie207 <adrienne.yao@gmail.com>
2020-11-20 10:51:03 -0500

    Added the subtask form with a subtask helper to show list of tasks to pick from

--- app/helpers/subtasks_helper.rb
+++ app/helpers/subtasks_helper.rb
@@ -1,2 +1,9 @@
 module SubtasksHelper
+    def task_id_field(subtask)
+        if subtask.task.nil?
+        select_tag "subtask[task_id]", options_from_collection_for_select(Task.all, :id, :title
+        else
+        hidden_field_tag "subtask[task_id]", subtask.task_id
+        end
+    end
 end
