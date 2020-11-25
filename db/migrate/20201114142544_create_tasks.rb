b310234487a5c8bb85cf12ce512bd28fa30ef7ee
yurigurlie207 <adrienne.yao@gmail.com>
2020-11-25 09:51:22 -0500

    Created form partial for subtask to use in new and edit pages

--- db/migrate/20201114142544_create_tasks.rb
+++ db/migrate/20201114142544_create_tasks.rb
@@ -2,9 +2,7 @@ class CreateTasks < ActiveRecord::Migration[6.0]
   def change
     create_table :tasks do |t|
       t.string :title
-      t.integer :estimated_duration
       t.date :deadline
-      t.string :priority
       t.text :notes
       t.boolean :complete
       t.timestamps null: false
--- db/migrate/20201114142704_create_subtasks.rb
+++ db/migrate/20201114142704_create_subtasks.rb
@@ -4,11 +4,8 @@ class CreateSubtasks < ActiveRecord::Migration[6.0]
       t.string :title
       t.integer :estimated_duration
       t.integer :actual_duration
-      t.date :deadline
       t.string :priority
       t.text :notes
-      t.text :feedback
-      t.boolean :no_subtask
       t.boolean :complete
       t.timestamps null: false
     end
