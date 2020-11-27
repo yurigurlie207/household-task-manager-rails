47a45ce65b2562413edc02605fbc0b4ff0cf7d84
yurigurlie207 <adrienne.yao@gmail.com>
2020-11-27 10:09:03 -0500

    Cleaned up and reogranized routes page

--- config/routes.rb
+++ config/routes.rb
@@ -1,36 +1,21 @@
 Rails.application.routes.draw do
+  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+  root 'static#home', as: 'home'
+  match '/auth/:provider/callback', to: 'sessions#create', via: [:get, :post]
+  get '/login', to: 'sessions#new', as: 'login'
+  post '/login', to: 'sessions#create'
+  post '/logout', to: 'sessions#destroy', as: 'logout'
 
   resources :users
-  
+
   resources :tasks do
     resources :subtasks
   end
 
   resources :subtasks
-  # delete '/subtasks/:id', to: 'subtasks#destroy', as: 'delete_subtask'
+
  
- 
-
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-
-  root 'static#home', as: 'home'
-  match '/auth/:provider/callback', to: 'sessions#create', via: [:get, :post]
-
-
-  # get '/users', to:'users#index', as: 'users'
-  # get '/users/new', to: 'users#new', as: 'new_user'
-  # post '/users', to:'users#create'
-  # get '/users/:id', to: 'users#show', as: 'userhome'
-  # get '/users/:id/edit', to: 'users#edit', as: 'edit_user'
-
-  get '/login', to: 'sessions#new', as: 'login'
-  post '/login', to: 'sessions#create'
-  post '/logout', to: 'sessions#destroy', as: 'logout'
-
-
-
-  
-
 end
 
 
