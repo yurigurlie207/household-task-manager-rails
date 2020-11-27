38b1543ab0cf7d361ff2c2d7500c0442590d0727
yurigurlie207 <adrienne.yao@gmail.com>
2020-11-27 09:56:13 -0500

    Added Delete User controller method

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,7 +5,6 @@ class UsersController < ApplicationController
   skip_before_action :require_login, only: [:index, :new, :create]
     #raise params.inspect/whatever.inspect for debugging
 
-    #this route is for development/testing purposes
     def index
     end
 
@@ -14,13 +13,12 @@ class UsersController < ApplicationController
     end
    
     def create
-      user = User.create(user_params)
-      render 'new' unless user.save
+      @user = User.create(user_params)
+      render 'new' unless @user.save
 
-      session[:user_id] = user.id
-      redirect_to '/users/' + user.id.to_s
+      session[:user_id] = @user.id
+      redirect_to @user
 
-      #try this later: redirect_to @user
     end
 
     def show
@@ -37,7 +35,7 @@ class UsersController < ApplicationController
     def update
       @user = User.find(params[:id])
 
-      @user.update(subtask_params)
+      @user.update(user_params)
 
       if @user.save
         redirect_to @user
@@ -45,101 +43,26 @@ class UsersController < ApplicationController
         render :edit
       end
     end
-    
+
+
+    def destroy
+      @user = User.find(params[:id])
+      @user.destroy
+      session.delete :user_id
+        # flash[:notice] = "Subtask deleted."
+        redirect_to '/'
+    end
+  
+
     private
   
     def user_params
-      params.require(:user).permit(:username, :firstname, :lastname, :role, :birthdate, :password, :password_confirmation)
+      params.require(:user).permit(:username, :firstname, :lastname, :email, :role, :birthdate, :password, :password_confirmation)
     end
    
     def require_login
       return head(:forbidden) unless session.include? :user_id
     end
-
-
-  # get '/user/signup' do
-  #   if !logged_in?
-  #     erb :'/user/signup'
-  #   else
-  #     redirect to '/user/userhome'
-  #   end
-  # end
-
-
-  # post '/signup' do
-  #   usernames = User.all.collect { |user| user.username }
-
-  #   if params['user']['username'] == "" || params['user']['password'] == ""
-  #     flash.now[:error] = "please make sure you have a username and password"
-  #     erb :'/user/signup'
-  #   elsif usernames.include?(params['user']['username'])
-  #     flash.now[:error] = "username already exists"
-  #     erb :'/user/signup'
-  #   else
-  #     @user = User.create(params["user"])
-  #     session[:user_id] = @user.id
-  #     redirect to "/user/#{@user.id}"
-  #   end
-  # end
-
-  # post '/login' do
-  #  user = User.find_by(:username => params['user']['username'])
-  #  if user && user.authenticate(params['user']['password'])
-  #    session[:user_id] = user.id
-  #    redirect to "/user/userhome"
-  #  else
-  #    flash.now[:error] = "incorrect login or password"
-  #    erb :'/user/login'
-  #  end
-  # end
-
-  # get '/user/:id/edit' do
-  #   if !logged_in?
-  #     erb :'/user/login'
-  #   else
-  #     @user = current_user
-  #     erb :'/user/edit'
-  #   end
-  # end
-
-  # patch '/user/:id' do
-  #   if !logged_in?
-  #     erb :'/user/login'
-  #   else
-  #     @user = current_user
-  #     @user.update(params['user'])
-  #     redirect to "/user/#{@user.id}"
-  #   end
-  # end
-
-  # delete '/user/:id/delete' do
-  #    if !logged_in?
-  #      erb :'/user/login'
-  #   else
-  #     @user = current_user
-  #     @user.delete
-  #     redirect to '/user/signup'
-  #   end
-  # end
-
-  # get '/user/logout' do
-  #     if logged_in?
-  #       session.destroy
-  #       redirect to '/user/login'
-  #     else
-  #       redirect to '/'
-  #     end
-  # end
-
-  # get '/user/:id' do
-  #   if !logged_in?
-  #     erb :'/user/login'
-  #    else
-  #      #This is really a route feature for debugging, so current user helper not used
-  #       @user = User.find_by_id(params[:id])
-  #       erb :'/user/profile'
-  #   end
-  # end
 
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
 
+  resources :users
+  
   resources :tasks do
     resources :subtasks
   end
@@ -15,11 +17,11 @@ Rails.application.routes.draw do
   match '/auth/:provider/callback', to: 'sessions#create', via: [:get, :post]
 
 
-  get '/users', to:'users#index', as: 'users'
-  get '/users/new', to: 'users#new', as: 'new_user'
-  post '/users', to:'users#create'
-  get '/users/:id', to: 'users#show', as: 'userhome'
-  get '/users/:id/edit', to: 'users#edit', as: 'edit_user'
+  # get '/users', to:'users#index', as: 'users'
+  # get '/users/new', to: 'users#new', as: 'new_user'
+  # post '/users', to:'users#create'
+  # get '/users/:id', to: 'users#show', as: 'userhome'
+  # get '/users/:id/edit', to: 'users#edit', as: 'edit_user'
 
   get '/login', to: 'sessions#new', as: 'login'
   post '/login', to: 'sessions#create'
