c6c13866d701c3aa7df327e51207fbb317b6dca3
yurigurlie207 <adrienne.yao@gmail.com>
2020-12-01 12:45:56 -0500

    Added raesoning for why user ids doesn't need to be checked for subtask

--- app/models/subtask.rb
+++ app/models/subtask.rb
@@ -8,6 +8,8 @@ class Subtask < ApplicationRecord
   validates :title, presence: true
   validates :notes, length: { maximum: 500 }
 
-  # validates_with SubtaskusersValidator
+  # validates_with SubtaskusersValidator - decided not to build this, because user_ids isn't really part of a subtask record
+  #also, functionally, there doesn't need to be a user assigned to a task or subtask, as it's just a decision 
+  # the user hasn't made yet. better to just highlight it maybe with stylesheets if no users assigned
 
 end
