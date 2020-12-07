65a3d6b9833b8cc61eb6d05e5d91d4820e539fd4
yurigurlie207 <adrienne.yao@gmail.com>
2020-12-07 06:26:04 -0500

    Deleted out some commented code in User model

--- app/models/user.rb
+++ app/models/user.rb
@@ -15,33 +15,5 @@ class User < ApplicationRecord
     validates :email, uniqueness: {
       message: 'another user already has this email'
     }
-    # validates :birthdate, date: { before: Proc.new { Time.now } }
-    
 
-   #  validates :title, presence: true
-   #  validates :title, uniqueness: {
-   #    scope: %i[release_year artist_name],
-   #    message: 'cannot be repeated by the same artist in the same year'
-   #  }
-   #  validates :released, inclusion: { in: [true, false] }
-   #  validates :artist_name, presence: true
-  
-   #  with_options if: :released? do |song|
-   #    song.validates :release_year, presence: true
-   #    song.validates :release_year, numericality: {
-   #      less_than_or_equal_to: Date.today.year
-   #    }
-   #  end
-  
-   #  def released?
-   #    released
-   #  end
-
-   # validates :name, length: { minimum: 2 }
-   # validates :bio, length: { maximum: 500 }
-   # validates :password, length: { in: 6..20 }
-   # validates :registration_number, length: { is: 6 }
-
-   # validates :not_a_robot, acceptance: true, message: "Humans only!"
-   # validates :category, inclusion: { in: %w(Fiction Non-Fiction) }
 end
