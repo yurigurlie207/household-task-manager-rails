9b3c40488bbad8785bc9ea98b09e6086561c2288
yurigurlie207 <adrienne.yao@gmail.com>
2020-12-13 11:12:45 -0500

    Updated subtask helpers to account for new path

--- app/helpers/subtasks_helper.rb
+++ app/helpers/subtasks_helper.rb
@@ -1,20 +1,14 @@
 module SubtasksHelper
     def estimated_select(subtask, path)
-        # binding.pry
-
-        if path =~ /.*new.*/
-            select_tag "subtask[estimated_duration]", options_for_select(['5 minutes','15 minutes', '30 minutes', '45 minutes', '60 minutes'], '5 minutes')
-        else
-            select_tag "subtask[estimated_duration]", options_for_select(['5 minutes','15 minutes', '30 minutes', '45 minutes', '60 minutes'], subtask.estimated_duration ? subtask.estimated_duration : '5 minutes')
-        end
+        select_tag "subtask[estimated_duration]", options_for_select(['5 minutes','15 minutes', '30 minutes', '45 minutes', '60 minutes'], path =~ /.*new.*/ ? '5 minutes' : subtask.estimated_duration)
     end
 
     def actual_select(subtask)
-        select_tag "subtask[actual_duration]", options_for_select(['5 minutes','10 minutes','30 minutes', '45 minutes', '60 minutes'], subtask.actual_duration ? subtask.actual_duration : '5 minutes')
+        select_tag "subtask[actual_duration]", options_for_select(['5 minutes','10 minutes','30 minutes', '45 minutes', '60 minutes'], path =~ /.*new.*/ ? '5 minutes' : subtask.actual_duration)
     end
 
     def priority_select(subtask)
-        select_tag "subtask[priority]", options_for_select(['low', 'medium', 'high'], subtask.priority ? subtask.priority : 'medium')
+        select_tag "subtask[priority]", options_for_select(['low', 'medium', 'high'], path =~ /.*new.*/ ? 'medium' : subtask.priority )
     end
 
     #return an array of usernames, only if there are users assigned
