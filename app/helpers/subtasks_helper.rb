3617102fc896a406c6a6c32485eb99603627a930
yurigurlie207 <adrienne.yao@gmail.com>
2020-12-15 22:01:59 -0500

    Made blank options for subtask dropdowns

--- app/helpers/subtasks_helper.rb
+++ app/helpers/subtasks_helper.rb
@@ -1,14 +1,14 @@
 module SubtasksHelper
     def estimated_select(subtask, path)
-        select_tag "subtask[estimated_duration]", options_for_select(['5 minutes','15 minutes', '30 minutes', '45 minutes', '60 minutes'], path !~ /.*edit.*/ ? '      ' : subtask.estimated_duration)
+        select_tag "subtask[estimated_duration]", options_for_select(['','5 minutes','15 minutes', '30 minutes', '45 minutes', '60 minutes'], path !~ /.*edit.*/ ? '      ' : subtask.estimated_duration)
     end
 
     def actual_select(subtask, path)
-        select_tag "subtask[actual_duration]", options_for_select(['5 minutes','10 minutes','30 minutes', '45 minutes', '60 minutes'], path !~ /.*edit.*/ ? '      ' : subtask.actual_duration)
+        select_tag "subtask[actual_duration]", options_for_select(['','5 minutes','10 minutes','30 minutes', '45 minutes', '60 minutes'], path !~ /.*edit.*/ ? '      ' : subtask.actual_duration)
     end
 
     def priority_select(subtask,path)
-        select_tag "subtask[priority]", options_for_select(['low', 'medium', 'high'], path !~ /.*edit.*/ ? '      ' : subtask.priority )
+        select_tag "subtask[priority]", options_for_select(['','low', 'medium', 'high'], path !~ /.*edit.*/ ? '      ' : subtask.priority )
     end
 
     #return an array of usernames, only if there are users assigned
