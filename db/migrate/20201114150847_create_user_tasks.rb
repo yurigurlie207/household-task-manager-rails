fdf6bb61410e4f4ddf9add5e016a9a9a3bc3e8e2
yurigurlie207 <adrienne.yao@gmail.com>
2020-12-30 10:09:22 -0500

    Added comment column to usertask table -  need to rerun migration

--- db/migrate/20201114150847_create_user_tasks.rb
+++ db/migrate/20201114150847_create_user_tasks.rb
@@ -3,6 +3,7 @@ class CreateUserTasks < ActiveRecord::Migration[6.0]
     create_table :user_tasks do |t|
       t.belongs_to :user, index: true, foreign_key: true
       t.belongs_to :subtask, index: true, foreign_key: true
+      t.text :comment
       t.timestamps null: false
     end
   end
